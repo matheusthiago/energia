2ae68bf908764286cafa8696a894b10ecdec4168
Win <Win@Win-PC>
2017-03-09 17:22:29 -0300

    Segurança + hash test 10

--- public_html/php/ psl-config.php
+++ public_html/php/ psl-config.php
@@ -2,7 +2,7 @@
 /**
  * Seguem os detalhes para login para o banco de dados
  */  
-define("HOST", "localhost");     // Para o host com o qual você quer se conectar.
+define("HOST", "192.168.0.100");     // Para o host com o qual você quer se conectar.
 define("USER", "sec_user");    // O nome de usuário para o banco de dados. 
 define("PASSWORD", "M73N3491");    // A senha do banco de dados. 
 define("DATABASE", "secure_login");    // O nome do banco de dados. 
