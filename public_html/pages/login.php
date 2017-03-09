a644ec81b8aea425802402ea223633787ae4aeff
Win <Win@Win-PC>
2017-03-09 17:00:29 -0300

    Segurança + hash test 5

--- public_html/pages/login.php
+++ public_html/pages/login.php
@@ -1,5 +1,5 @@
 <?php
-/*include_once '../php/db_connect.php';
+include_once '../php/db_connect.php';
 include_once '../php/functions.php';
  
 sec_session_start();
@@ -10,15 +10,16 @@ if (login_check($mysqli) == true) {
 } else {
     echo 'não logado com sucesso bd';
     $logged = 'out';
-}*/
+}
 ?>
 <!DOCTYPE html>
-<html>
+<html> 
+    
     <head>
         <title>Secure Login: Log In</title>
         <link rel="stylesheet" href="styles/main.css" />
-        <script type="text/JavaScript" src="js/sha512.js"></script> 
-        <script type="text/JavaScript" src="js/forms.js"></script> 
+        <script type="text/JavaScript" src="../js/sha512.js"></script> 
+        <script type="text/JavaScript" src="../js/forms.js"></script> 
     </head>
     <body>
         <?php
@@ -36,7 +37,7 @@ if (login_check($mysqli) == true) {
                    onclick="formhash(this.form, this.form.password);" /> 
         </form>
         <p>If you don't have a login, please <a href="register.php">register</a></p>
-        <p>If you are done, please <a href="../../php/logout.php">log out</a>.</p>
+        <p>If you are done, please <a href="../php/logout.php">log out</a>.</p>
         <p>You are currently logged <?php echo $logged ?>.</p>
     </body>
 </html>
