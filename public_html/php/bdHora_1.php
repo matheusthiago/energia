4c8461cee908caa4e12604b872248b5361cb74af
Win <Win@Win-PC>
2017-03-31 15:00:03 -0300

    grafico

--- public_html/php/bdHora_1.php
+++ public_html/php/bdHora_1.php
@@ -17,22 +17,27 @@ $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-for($i=0;$i<=20;$i++){
-$sql = "select (hour(horario)) as hora,(day(horario)) as dia,(month(horario)) as mes,(year(horario)) as ano,
-             (SUM(potencia)/(3600000) AS kwh, HOUR(horario) as hora 
-            from medidas where DATE(horario)=(DATE_ADD(CURDATE(), INTERVAL -1 DAY)) GROUP BY hora ASC";
+$sql = "select (hour(horario)) as hora,"
+        . "(day(horario)) as dia,"
+        . "(month(horario)) as mes,"
+        . "(year(horario)) as ano,"
+        . "(SUM(potencia)/(3600000)) AS kwh,"
+        . "HOUR(horario) as hora"
+        . " from medidas "
+        . "where date(horario) between '2017-02-20' and curdate() "
+        . "group by hour(horario) , date(horario)";
 
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
+        $hora = menorQue9($row["hora"]);
         $dia = menorQue9($row["dia"]);
         $mes = menorQue9($row["mes"]);
-        $hora = menorQue9($row["hora"]);
         $ano = menorQue9($row["ano"]);
         $id = $hora . $dia . $mes . $ano;
         $kwh = $row["kwh"];
         echo "id: " . $id . "kwh:" . $kwh . "<br>";
-        $insert = "insert into medidasHora (id,potencia) values ('" . $id . "','" . $kwh . "')";
+        $insert = "insert into medidasHora(id,potencia, hora, dia, mes, ano) values ('','" . $kwh . "','" . $hora . "','" . $dia . "','" . $mes . "','" . $ano . "')";
 
         if ($conn->query($insert) == TRUE) {
             echo "\n Salvo com Sucesso";
@@ -48,4 +53,5 @@ $conn->close();
 function menorQue9($valor) {
     return $valor < 10 ? '0' . $valor : $valor;
 }
+
 ?>
