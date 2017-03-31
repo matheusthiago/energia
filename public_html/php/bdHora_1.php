0e02afbe75c95b437959bf3417a57975b64c55d2
pc <pc@pc-PC>
2017-03-30 21:56:40 -0300

    salvar em novo bd

--- public_html/php/bdHora_1.php
+++ public_html/php/bdHora_1.php
@@ -1,5 +1,5 @@
 <?php
-
+for($i=0; $i<20; $i++){
 //setting header to json
 header('Content-Type: application/json');
 
@@ -16,7 +16,7 @@ $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-for($i=1; $i<20; $i++){
+
 $sql = "select (hour(horario)) as hora,(day(horario)) as dia,(month(horario)) as mes,(year(horario)) as ano,
              ROUND(SUM(potencia)/(3600000),4) AS kwh, HOUR(horario) as hora 
             from medidas where DATE(horario)=(DATE_ADD(CURDATE(), INTERVAL -".$i." DAY)) GROUP BY hora ASC";
@@ -50,6 +50,10 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
-}
+$sql='';
+$insert='';
+
 $conn->close();
+
+}
 ?>
