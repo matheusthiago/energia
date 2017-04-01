8be44286044a599745c33075f27bcfaa35d4218b
pc <pc@pc-PC>
2017-04-01 12:16:00 -0300

    js

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
                 data: {
                     labels: label,
                     datasets: [{
-                            label: 'Consumo total no dia é:'+ preco.toFixed(2)+'R$',
+                            label: 'Consumo total no dia é:'+ preco.toFixed(2)+' R$',
                             backgroundColor: 'rgba(91, 184, 93, 0.75)',
                             borderColor: 'rgba(200, 200, 200, 0.75)',
                             hoverBackgroundColor: 'rgba(91, 184, 93, 1)',
