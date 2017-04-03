b49b1cf7222e50d5b9bee8adf83c2a1a39bd2c97
pc <pc@pc-PC>
2017-04-03 11:27:02 -0300

    erro com a consulta

--- public_html/php/data2_1.php
+++ public_html/php/data2_1.php
@@ -23,7 +23,7 @@ if (!$mysqli) {
 
 //$query = sprintf("SELECT ROUND(SUM(potencia)*(0.62378769 ),2) AS preco, `hora`, CONCAT(`dia`,'-',`mes`,'-',`ano`) AS datas FROM medidasHora WHERE (DATE_ADD(STR_TO_DATE((CONCAT(`ano`,',',`mes`,',',`dia`,' ',`hora`)),'%Y,%m,%d %H'), INTERVAL 1 HOUR))>=(DATE_SUB(NOW(), INTERVAL 1 DAY)) GROUP BY `dia`, `hora`");
 //$query=sprintf("SELECT ROUND(SUM(potencia)*(0.62378769 ),2) AS preco, `hora`, CONCAT(`dia`,'-',`mes`,'-',`ano`) AS datas FROM medidasHora WHERE ((STR_TO_DATE((CONCAT(`ano`,',',`mes`,',',`dia`,' ',`hora`)),'%Y,%m,%d %H')))>=(DATE_SUB(NOW(), INTERVAL 1 DAY)) GROUP BY `dia`, `hora`");
-$query=sprintf("SELECT ROUND(SUM(potencia)*(0.62378769 ),2) AS preco, `hora`, (CONCAT(`ano`,`mes`,`dia`,`hora`)) AS DATA FROM medidasHora GROUP BY `dia`, `hora`");
+$query=sprintf("SELECT ROUND(SUM(potencia)*(0.62378769 ),2) AS preco, `hora`, (CONCAT(`ano`,`mes`,`dia`,`hora`)) AS DATA, STR_TO_DATE('05/18/2009', '%m/%d/%Y') FROM medidasHora GROUP BY `dia`, `hora`");
 
 $result = $mysqli->query($query) or die(mysqli_error());
 
