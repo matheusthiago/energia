687b6adb406c116f363e721fd0d0855ac50cc01a
pc <pc@pc-PC>
2017-04-03 12:41:09 -0300

    ajeitando os gráficos

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
                         mode: 'point',
                         callbacks: {
                             beforeTitle: function(){
-                              return "tent"+datasetIndex; 
+                              return "tent"; 
                             },
                             title: function () {
                                 return config.data.datasets.label;
