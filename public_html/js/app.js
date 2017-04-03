ec125558a7934fb6e07a3833f290ac28cda5c43e
pc <pc@pc-PC>
2017-04-03 13:43:14 -0300

    ajeitando os gráficos

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
                     },
                     title: {
                         display: true,
-                        text: 'Total gasto na hora: R$' + preco.toFixed(2)
+                        text: 'Total gasto em ' + data[0].datas +' - '+data[data.length-1].datas+': R$' + preco.toFixed(2)
                     },
                     tooltips: {
                         mode: 'point',
