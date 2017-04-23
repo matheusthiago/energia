b332ad7c647ee358e2680b98f8f5d1917b4a79bb
mathe <mathe@DESKTOP-F3S1324>
2017-04-23 11:06:27 -0300

    '

--- public_html/Interface-Web-MQTT-master/tmo.js
+++ public_html/Interface-Web-MQTT-master/tmo.js
@@ -43,7 +43,7 @@
 			};
 			mosq.ondisconnect = function(rc){
 				var p = document.createElement("p");
-				var url = "ws://192.168.0.105";
+				var url = "ws://192.168.0.100'";
 				
 				p.innerHTML = "A conexão com o broker foi perdida";
 				$("#debug").append(p);				
@@ -63,7 +63,7 @@
 			};
 		}
 		Page.prototype.connect = function(){
-			var url = "ws://192.168.0.105";
+			var url = "ws://192.168.0.100";
 			mosq.connect(url);
 		};
 		Page.prototype.disconnect = function(){
