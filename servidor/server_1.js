6831db5429e7a5aabee8936b4f8fb45ce2b81587
Dell <Dell@DESKTOP-0E4013O>
2017-05-18 20:34:31 -0300

    error bd casa

---
+++ servidor/server_1.js
@@ -0,0 +1,31 @@
+/** parte que faz a conex�o com o banco de dados */
+var host = 'localhost';
+var user = 'root';
+var password = 'r007';
+var database = 'energy';
+var table = 'medidas';
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: host,
+    user: user,
+    password: password,
+    database: database
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    } else
+        console.log('database is connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM employees', function(err, rows, fields)   
+{  
+  if (err) throw err;  
+  
+  console.log(rows[0]);  
+});  
+  
+connection.end();  
