dccd8d207b722226de5dd33c087f5e2311b0baf2
Dell <Dell@DESKTOP-0E4013O>
2017-06-12 18:38:49 -0300

    Dados do mês de maio

--- public_html/php/data.php
+++ public_html/php/data.php
@@ -13,8 +13,8 @@ if (!$mysqli) {
     die("Connection failed: " . $mysqli->error);
 }
 //$query = sprintf("SELECT concat(dia,'/',mes) as diaMes, dia, round((sum(potencia))*(0.62378769),2) as preco from medidasHora group by dia, mes, ano order by ano asc, mes asc, dia asc"); //Ultimos 30 dias
-$query = sprintf("SELECT concat(dia,'/',mes) as diaMes, dia, ROUND(SUM(potencia)*(0.58847192),2) AS preco, sum(potencia) AS pot  FROM medidasHora WHERE (mes=04 and dia>=07) OR (mes=5 and dia<=9)group by dia, mes, ano order by ano asc, mes asc, dia asc"); // mes 4-5
-//$query= sprintf("SELECT concat(dia,'/',mes) as diaMes, dia, ROUND(SUM(potencia)*(0.61342401),2) AS preco, sum(potencia) AS pot FROM medidasHora WHERE (mes=03 and dia>=09) OR (mes=4 and dia<=07)group by dia, mes, ano order by ano asc, mes asc, dia asc"); // mes 3-4
+//$query = sprintf("SELECT concat(dia,'/',mes) as diaMes, dia, ROUND(SUM(potencia)*(0.58847192),2) AS preco, sum(potencia) AS pot  FROM medidasHora WHERE (mes=04 and dia>=07) OR (mes=5 and dia<=9)group by dia, mes, ano order by ano asc, mes asc, dia asc"); // mes 4-5
+$query= sprintf("SELECT concat(dia,'/',mes) as diaMes, dia, ROUND(SUM(potencia)*(0.61342401),2) AS preco, sum(potencia) AS pot FROM medidasHora WHERE (mes=03 and dia>=09) OR (mes=4 and dia<07)group by dia, mes, ano order by ano asc, mes asc, dia asc"); // mes 3-4
 //$query= sprintf("SELECT concat(dia,'/',mes) as diaMes, dia, ROUND(SUM(potencia)*(0.61342401),2) AS preco, sum(potencia) AS pot FROM medidasHora WHERE (mes=05 and dia>=09) OR (mes=6 and dia<=08)group by dia, mes, ano order by ano asc, mes asc, dia asc"); // mes 5-6
 
 $result = $mysqli->query($query);
