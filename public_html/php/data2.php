3683441c209af610e8b85c62de43c084d4722f3c
MT <MT@DESKTOP-5GMBILA>
2017-11-07 18:14:42 -0200

    agrupamento ultimos 16 dias

--- public_html/php/data2.php
+++ public_html/php/data2.php
@@ -14,7 +14,8 @@ if (!$mysqli) {
     die("Connection failed: " . $mysqli->error);
 }
 //query to get data from the table
-$query = "SELECT ROUND(SUM(potencia)*(0.62378769 ),2) AS preco, concat(hora,':00') as hora, CONCAT(`dia`,'-',`mes`,'-',`ano`) AS datas FROM medidasHora WHERE (DATE_ADD(STR_TO_DATE((CONCAT(`ano`,',',`mes`,',',`dia`,' ',`hora`)),'%Y,%m,%d %H'), INTERVAL 1 HOUR))>=(DATE_SUB(NOW(), INTERVAL 15 DAY)) GROUP BY `dia`, `hora`";
+//$query = "SELECT ROUND(SUM(potencia)*(0.62378769 ),2) AS preco, concat(hora,':00') as hora, CONCAT(`dia`,'-',`mes`,'-',`ano`) AS datas FROM medidasHora WHERE (DATE_ADD(STR_TO_DATE((CONCAT(`ano`,',',`mes`,',',`dia`,' ',`hora`)),'%Y,%m,%d %H'), INTERVAL 1 HOUR))>=(DATE_SUB(NOW(), INTERVAL 15 DAY)) GROUP BY `dia`, `hora`";
+//$query = "select sum(potencia) as potencia, concat(hora,':00') as hora, CONCAT(`dia`,'-',`mes`,'-',`ano`) AS datas FROM medidasHora where mes>08 and dia>07 and dia<24 group by `dia`, `hora`";
 $result = $mysqli->query($query) or die(mysqli_error());
 //loop through the returned data
 $data = array();
